befd8960177833f3ecf7aa3ca8699df37c0b90a2
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-10-03 12:34:30 +0530

    fix: demo commit

--- .releaserc.js
+++ .releaserc.js
@@ -5,12 +5,12 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
       "@semantic-release/changelog",
-      [
-        "@semantic-release/exec",
-        {
-          prepareCmd: "npx lerna version --conventional-commits --yes",
-        },
-      ],
+    //   [
+    //     "@semantic-release/exec",
+    //     {
+    //       prepareCmd: "npx lerna version --conventional-commits --yes",
+    //     },
+    //   ],
       [
         "@semantic-release/git",
         {
